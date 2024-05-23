33bd76cdf15f60fadc537d78d1494df177351df4
Martin Nepita <35706069+mnepita@users.noreply.github.com>
2024-05-23 03:00:47 +0000

    operadores en python

--- Basic/02_operators.py
+++ Basic/02_operators.py
@@ -14,7 +14,12 @@ print(3 + 6)
 print(3 - 6)
 print(3 * 6)
 print(3 / 6)
-print(3 % 6) #modulo restante
+print(3 % 6) # modulo restante
+print(10 // 3) # floor division, calcula cuantas veces cabe, siempre devuelve entero
+print( 2 ** 3 ) # exponentes 
+
+print("Hola " * 5) # imprime 5 veces hola
+print("Hola " + str(5))
 
 
 # Floating numbers
@@ -24,3 +29,33 @@ print('Floating Point Number, gravity', 9.81)
 # Complex numbers
 print('Complex number: ', 1 + 1j)
 print('Multiplying complex numbers: ',(1 + 1j) * (1 - 1j))
+
+
+### OPERADORES ARITMETICOS ###
+print(3 > 4)
+print(3 < 4)
+print(3 >= 4)
+print(4 <= 4)
+print(3 == 4)
+print(3 != 4)
+
+## Operaciones con cadenas de texto
+print("Hola" > "Python")
+print("Hola" < "Python")
+print("aaaa" >= "abaa")  # Ordenación alfabética por ASCII
+print(len("aaaa") >= len("abaa"))  # Cuenta caracteres
+print("Hola" <= "Python")
+print("Hola" == "Hola")
+print("Hola" != "Python")
+
+### Operadores logicos ###
+
+# Basada en el algebra de Boole https://es.wikipedia.org/wiki/%C3%81lgebra_de_Boole
+print(3 > 4 and "Hola" > "Python")
+print(3 > 4 or "Hola" > "Python")
+print(3 < 4 and "Hola" < "Python")
+print(3 < 4 or "Hola" > "Python")
+print(3 < 4 or ("Hola" > "Python" and 4 == 4))
+print(not (3 > 4))
+
+
